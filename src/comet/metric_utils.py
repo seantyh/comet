6827b9cbb7d15e4a8c187126cdb6e7c7c3679c28
seantyh <seantyh@gmail.com>
2023-01-18 16:11:02 +0800

    add device arg to metric

--- src/comet/metric_utils.py
+++ src/comet/metric_utils.py
@@ -7,7 +7,8 @@ from torch.utils.data import DataLoader
 
 def compute_knn_accuracies(model,
     loader: DataLoader, 
-    knn: NearestNeighbors):
+    knn: NearestNeighbors, 
+    device: str="cpu"):
   """Compute the top-k accuracies of the model using k-nearest neighbors."""
 
   model.eval()
@@ -16,10 +17,10 @@ def compute_knn_accuracies(model,
     n_correct = 0
     topk_corrects = np.zeros((15,)) 
     for batch in tqdm(loader, desc="Computing knn accuracy"):
-      consts = batch["consts"]
-      compound = batch["compound"]
+      consts = batch["consts"].to(device)
+      compound = batch["compound"].to(device)
       pred = model(consts)
-      _, idx = knn.kneighbors(pred)
+      _, idx = knn.kneighbors(pred.cpu())
       batch_true_idxs = batch["idx"].numpy()
       for k in range(15):
         correct_mat = (idx[:, :k+1] == batch_true_idxs[:, np.newaxis])
