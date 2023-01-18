90154894a6f510fdefc180828c5d6e245fc21fca
seantyh <seantyh@gmail.com>
2023-01-18 15:36:40 +0800

    add metric_utils.py

--- src/comet/__init__.py
+++ src/comet/__init__.py
@@ -1,1 +1,2 @@
-from compound_eps import CompoundEps
+from .compound_eps import CompoundEps
+from .metric_utils import compute_knn_accuracies
---
+++ src/comet/metric_utils.py
@@ -0,0 +1,28 @@
+from tqdm.auto import tqdm
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+import torch
+from torch.utils.data import DataLoader
+
+
+def compute_knn_accuracies(model,
+    loader: DataLoader, 
+    knn: NearestNeighbors):
+  """Compute the top-k accuracies of the model using k-nearest neighbors."""
+
+  model.eval()
+  n_instances = len(loader.dataset)   # type: ignore
+  with torch.no_grad():
+    n_correct = 0
+    topk_corrects = np.zeros((15,)) 
+    for batch in tqdm(loader, desc="Computing knn accuracy"):
+      consts = batch["consts"]
+      compound = batch["compound"]
+      pred = model(consts)
+      _, idx = knn.kneighbors(pred)
+      batch_true_idxs = batch["idx"].numpy()
+      for k in range(15):
+        correct_mat = (idx[:, :k+1] == batch_true_idxs[:, np.newaxis])
+        topk_corrects[k] += correct_mat.sum().item()
+  topk_corrects = [x/n_instances for x in topk_corrects]
+  return topk_corrects
